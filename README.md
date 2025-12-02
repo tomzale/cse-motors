7ed9ddc62248fdc2f768aca5a7923851cdbe4436
Thomas Etiuzale <faithfulmissioner@gmail.com>
2025-12-02 22:02:08 +0100

    Initial commit - CSE Motors

---
+++ README.md
@@ -0,0 +1,7 @@
+# cse-motors
+
+Minimal Express + EJS app scaffold for CSE Motors.
+
+To run:
+1. npm install
+2. npm start
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
